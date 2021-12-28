e0e7b46672a105cc4631fb8cd7de2096f23e7e1a
bayungrh <bnugraha00@gmail.com>
2021-12-29 00:45:22 +0700

    ⚡fix local db

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const memoryAdapter = require('./memoryAdapter');
 const fileAdapter = require('./fileAdapter');
 
 module.exports = (options) => {
   if (options && options.persistence) {
-    const db = options.db ? options.db : 'cache.json';
+    const db = options.db ? options.db : path.resolve(__dirname, 'cache.json');
     return fileAdapter(db);
   } return memoryAdapter();
 }
